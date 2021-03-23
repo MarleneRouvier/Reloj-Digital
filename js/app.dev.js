c831512126e0bb5e8a0d83f7a14983b862808d5b
Marlene Rouvier <marlene_rouvier@hotmail.com>
2021-03-23 13:41:31 -0300

    first commit

---
+++ js/app.dev.js
@@ -0,0 +1,16 @@
+const reloj = document.querySelector(".reloj");
+
+function getHora() {
+    const fecha = new Date();
+
+    const tiempo = {
+        hora: fecha.getHours(),
+        minuto: fecha.getMinutes(),
+        segundo: fecha.getSeconds(),
+    };
+
+    reloj.innerHTML = `${tiempo.hora} : ${tiempo.minuto} : ${tiempo.segundo}`;
+
+};
+
+setInterval(getHora, 300);
